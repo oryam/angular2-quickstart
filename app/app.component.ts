4d9f4309f3ae67ea3d1507d5770f4ec98abea078
Amaury <abertron@kiween.com>
2016-02-17 19:39:17 +0100

    Initial files

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core'
+@Component({
+  selector: 'my-app',
+  template: `<h1>My component App</h1>`,
+  styles: [
+    `.my-app {}`
+  ]
+})
+export class AppComponent {
+}
